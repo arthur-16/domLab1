e2fff5d6d40a42990fb90b72a9523fb976eaf6a0
arthur16 <artbscott16@gmail.com>
2018-05-15 11:06:45 -0500

    third commit

--- dom.js
+++ dom.js
@@ -8,30 +8,24 @@ document.addEventListener("DOMContentLoaded", function() {
   btn.appendChild(btnDiv);
   document.body.appendChild(btnDiv);
   let divCounter = 0;
-  btn.addEventListener("click", function() {
+  btn.addEventListener("click", function(e) {
     let counter = listItemCounter++;
     let div1 = document.createElement("div");
     div1.classList.add("boxes");
     document.body.appendChild(div1);
-    let list = document.getElementsByClassName("boxes");
-    for (var i = 0; i < list.length; i++) {
-      list[i].setAttribute("id", "box-" + i);
-      let divList = document.createTextNode("id = box-" + i)
-      div1.appendChild(divList)
-      document.body.appendChild(div1)
-    }
+      let j = 1
+          div1.setAttribute("id", 'box- ' + j++);
 
-    // 1.
-
-
-
-
-    // div1.addEventListener("mouseover", function(p) {
-    //     console.log(p.target);
-            
-    //     div1.addEventListener("mouseout", function() {
-    //       this.style.color = "black";
-        // });
-    // });
+    div1.addEventListener("mouseover", function(ev) {
+      let boxID = ev.target.id;
+      let div1Text = document.createTextNode(boxID);
+      div1.appendChild(div1Text);
+      document.body.appendChild(div1);
+      div1.addEventListener("mouseout", function(ev) {
+        this.removeChild(div1Text);
+      });
+    });
   });
 });
+ // Square Hover objective
+ 
