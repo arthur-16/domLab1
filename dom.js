260cc066c5851300ad69c5e5d411b342e7d1294a
arthur16 <artbscott16@gmail.com>
2018-05-16 11:01:58 -0500

    third commit?

--- dom.js
+++ dom.js
@@ -13,19 +13,42 @@ document.addEventListener("DOMContentLoaded", function() {
     let div1 = document.createElement("div");
     div1.classList.add("boxes");
     document.body.appendChild(div1);
-      let j = 1
-          div1.setAttribute("id", 'box- ' + j++);
-
-    div1.addEventListener("mouseover", function(ev) {
-      let boxID = ev.target.id;
-      let div1Text = document.createTextNode(boxID);
-      div1.appendChild(div1Text);
-      document.body.appendChild(div1);
-      div1.addEventListener("mouseout", function(ev) {
-        this.removeChild(div1Text);
-      });
+    var i = ((j = 1), counter);
+    div1.setAttribute("id", "box- " + i);
+    var colors = [
+      "black",
+      "green",
+      "red",
+      "blue",
+      "yellow",
+      "purple",
+      "orange"
+    ];
+    div1.addEventListener("click", function() {
+      let rand = Math.floor(Math.random() * 7);
+      div1.style.backgroundColor = colors[rand];
     });
+    div1.addEventListener("dblclick", function() {
+      let even = (i % 2 === 0)
+      let odd = (i & 2 != 0)
+      if (even) {
+        div1.remove()
+      }
+      if (odd) {
+        div1.lastChild.remove()
+      }
+    }
+  )
   });
 });
- // Square Hover objective
- 
+// Square Hover objective
+// Remove element objective
+// div1.addEventListener("mouseover", function(ev) {
+//   let boxID = ev.target.id;
+//   let div1Text = document.createTextNode(boxID);
+//   div1.appendChild(div1Text);
+//   document.body.appendChild(div1);
+//   div1.addEventListener("mouseout", function(ev) {
+//     this.removeChild(div1Text);
+//   });
+// });
